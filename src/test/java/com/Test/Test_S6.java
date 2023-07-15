f447e19755f5f16e96ebe3db9e43278a840128ec
GovindPatil75 <mulegr75@gmail.com>
2023-07-15 09:33:14 +0530

    Xpath

---
+++ src/test/java/com/Test/Test_S6.java
@@ -0,0 +1,31 @@
+package com.Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test_S6 {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		
+		//Element identify -- Xpath --absolute XPath 
+		// Navigate Parent --immidiate child node -- Element 
+		
+		WebElement email=driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[1]/input"));
+		email.sendKeys("Test@gmail.com");
+		
+		WebElement password=driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[2]/div/input"));
+		password.sendKeys("12345678");
+		
+		
+
+	}
+
+}
