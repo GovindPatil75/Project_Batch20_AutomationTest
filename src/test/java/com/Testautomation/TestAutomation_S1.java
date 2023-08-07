3840d1bc2c629d4359849cf316f89ce6fb63de02
GovindPatil75 <mulegr75@gmail.com>
2023-08-07 09:26:12 +0530

    TestNG Session 1

---
+++ src/test/java/com/Testautomation/TestAutomation_S1.java
@@ -0,0 +1,24 @@
+package com.Testautomation;
+
+import org.testng.annotations.Test;
+
+public class TestAutomation_S1 {
+	
+	@Test
+    public void verifyTest1() {
+		
+		System.out.println("Hello World");
+	}
+	
+	@Test
+	public void verifyTest2() {
+		
+		System.out.println("BugSpotter");
+	}
+	
+	@Test
+	public void verifyTest3() {
+		
+		System.out.println("Test 3");
+	}
+}
